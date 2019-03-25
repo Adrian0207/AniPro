bef39d64bec3b3d10b581046f01f60765139dd8c
Francisco Mejia <fmejia928@outlook.com>
2019-03-25 18:59:11 -0500

    ux commit
    Co-Authored-By: @Francisco mejia <fmejia928@outlook.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,39 @@
+<style media="screen">
+  .im{
+    width: 300px;
+    height: 100px;
+    margin-left: 45%;
+
+  }
+  .imT{
+    width: 300px;
+    height: 100px;
+    margin-left: 39%;
+    margin-top: 90px;
+    margin-bottom: 50PX;
+  }
+</style>
 @extends('layouts.app')
 
+
 @section('content')
+<div class="im">
+    <img src="../images/logoA.jpeg" width="200px" height="200px" alt="">
+</div>
+<div class="imT">
+    <img src="../images/AnipT.png" width="400px" height="150px" alt="">
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +47,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -45,7 +66,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recordarme') }}
                                     </label>
                                 </div>
                             </div>
@@ -54,12 +75,12 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Ingresar') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Olvidaste tu Contraseña?') }}
                                     </a>
                                 @endif
                             </div>
