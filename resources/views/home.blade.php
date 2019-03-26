767590016f766fbf8a9c0d5ca84dc764b58f78bf
Adrian Oliva <adrian69oliva@gmail.com>
2019-03-25 22:22:28 -0500

    Link de adopcion en el inicio de sesion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a href="/adopcion">Adoptar</a>
                 </div>
             </div>
         </div>
